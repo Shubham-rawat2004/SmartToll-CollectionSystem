4d8c51605bcddfeecb446e303be504552e2b5dc2
Shubham Rawat <shubhamrawat1808@gmail.com>
2026-04-12 21:43:15 +0530

    Delete Git-Practice directory

--- Git-Practice/snc/src/main/java/com/smarttoll/restapi/product/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.smarttoll.restapi.product;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(Long id) {
-        super("Product not found with id: " + id);
-    }
-}
--- Git-Practice/src/main/java/com/example/gitpractice/model/Task.java
+++
@@ -1,4 +0,0 @@
-package com.example.gitpractice.model;
-
-public record Task(Long id, String title, String description, boolean completed) {
-}
